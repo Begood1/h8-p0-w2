81596fbc12c0b9690f1956a76a6063fb566d2a32
Begood1 <geze1010@gmail.com>
2018-07-07 17:54:38 +0700

    exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -1,42 +1,48 @@
-var name = 'sami';
-var role = 'witch';     
+var date = 2;
+var month = 10;
+var year = 2018;
+var result = date + ' ' + month + ' '+ year;
 
- if(name ==='' && role ===''){
-console.log('Name must be filled');
+if(date > 1 && date < 32) {
+switch(month) {
+     case 1:  { month = 'january'; 
+      result = date + '  ' + month + ' ' + year;  break;}
+        
+    case 2:    { month = 'february'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+
+    case 3:    { month = 'march'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+
+    case 4:    { month = 'april'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+    case 5:    { month = 'may'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+    case 6:    { month = 'june'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+    case 7:    { month = 'july'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+    case 8:    { month = 'augest'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+    case 9:    { month = 'september';
+      result = date + ' ' + month + ' ' + year;  break;}
+    case 10:    { month = 'october'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+    case 11:    { month = 'november'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+    case 12:    { month = 'december'; 
+     result = date + ' ' + month + ' ' + year;  break;}
+    default:     { result  = 'no file Available';}
+}
+    
+
+}else
+    
+{
+    result = 'no file available'
 }
 
-else if(name === 'geze' && role === ''){
-    console.log('Hello Geze, Choose your role to start the game!');
+    console.log(result);
 
-}
-else if(name === ''|| role === 'Knight'){
-   console.log('wellcome to proxy world sami');
-   console.log('Hello Nina Knight, you can attack with your weapons!');
-}else if(name === 'nina'|| role ==='Doctor'){
-    console.log('Welcome to Proxytia World, Doctor');
-    console.log('Hello Doctor sami, you will help your injured friend');
 
-}else if (name !== '' || role === 'witch') {
-     console.log('Welcome to Proxytia World,John')
-     console.log(' Hello john !, create a miracle that helps your victory!')
-}
 
- 
-
-// Output for Input name = '' and role = '' 
-//" Name must be filled! "
-
-// Output for Input name = 'Mikael' and role = '' 
-//" Hello Mikael, Choose your role to start the game! "
-
-// Output for Input name = 'Nina' and role 'Knight' 
-//" Welcome to Proxytia World, Nina " 
-//" Hello Nina Knight, you can attack with your weapons! "
-
-// Output for Input Name = 'Danu' and the role of 'Doctor' 
-//" Welcome to Proxytia World, Danu " 
-//" Hello Doctor Danu, you will help your injured friend. "
-
-// Output for Input Name = 'Zero' and the role of 'Wizard' 
-//" Welcome to Proxytia World, Zero " 
-//" Hello Wizard Zero, create a miracle that helps your victory! "
