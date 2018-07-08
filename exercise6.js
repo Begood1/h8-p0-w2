68c6ede5d019588c59ad69931f88fefffc626c84
Begood1 <geze1010@gmail.com>
2018-07-09 01:37:30 +0700

    week1html.png

---
+++ exercise6.js
@@ -0,0 +1,38 @@
+//exercise6 proplem 1
+var count = 2;
+console.log('LOOPING PERTAMA');
+    while (count  <= 20) {
+     
+      console.log(count ++,   '  - I love coding ');
+     count++;
+    }
+ console.log('LOOPING KEDUA')  
+
+
+
+var count = 20;
+while (count >= 1){
+console.log(count--,' - I will become fullstack developer');
+count--;
+}
+
+
+
+//This is for loop
+//exercise6 proplem 2
+
+console.log('LOOPING PERTAMA')
+for(var count = 1; count <= 20; count++) {
+  console.log(count+ ' -  I love coding');
+}
+
+
+console.log('LOOPING DUA')
+for(var count = 20; count > 0; count--) {
+
+  console.log(count+ ' -   I will become fullstack developer');
+  
+}
+
+ 
+
